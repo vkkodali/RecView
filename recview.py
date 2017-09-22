5a3fc6a821c574c32aea5b5cad6316a9bd459c24
Kodali <kodalivk@ncbi.nlm.nih.gov>
2017-09-22 13:19:54 -0400

    Add option to display zero-based count; add option to display headerless files

--- recview.py
+++ recview.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
 import sys
 import csv
@@ -10,6 +10,10 @@ field''')
 parser.add_argument('-i', '--input', help='input file, default is stdin')
 parser.add_argument('-d', '--delimiter', help='character used for delimiter;'
 					'default is tab')
+parser.add_argument('-z', '--zero_based', help='start numbering from zero',
+					action='store_true')
+parser.add_argument('-nh', '--noheader', help='no header present',
+					action='store_true')
 
 args = parser.parse_args()
 infile = args.input
@@ -19,23 +23,38 @@ from signal import signal, SIGPIPE, SIG_DFL
 signal(SIGPIPE, SIG_DFL)
 
 def recview():
-	header = next(tbl)
-	l = 0
-	for item in header:
-		if l < len(item):
-			l = len(item)
-	for row in tbl:
-		i = 1
-		j = 0
-		while (j < len(row) ):
-			print("{:>{}} [{:3d}]: {}" .format(header[j], l, i, row[j]))
-			i, j = i+1, j+1
-		print('\n')
+	if not args.noheader:
+		header = next(tbl)
+		l = 0
+		for item in header:
+			if l < len(item):
+				l = len(item)
+		for row in tbl:
+			i = z
+			j = 0
+			while (j < len(row) ):
+				print("{:>{}} [{:3d}]: {}" .format(header[j], l, i, row[j]))
+				i, j = i+1, j+1
+			print('\n')
+	else:
+		for row in tbl:
+			i = z
+			j = 0
+			while (j < len(row) ):
+				print("[{:3d}] {}" .format(i, row[j]))
+				i, j = i+1, j+1
+			print('\n')
+
 
 if args.delimiter:
 	delim = args.delimiter
 else:
 	delim = '\t'
+
+if args.zero_based:
+	z = 0
+else:
+	z = 1
 
 if args.input:
 	with open(infile, 'r') as f:
